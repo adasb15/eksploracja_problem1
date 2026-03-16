08dc3c2e6b4588e72c9989f9317ff91ebe7fd5bb
aleksandrabamberska <ola_bamberska@wp.pl>
2026-03-16 21:27:57 +0100

    Dodanie biasu

--- src/system155898.py
+++ src/system155898.py
@@ -1,25 +1,34 @@
 import numpy as np
-import csv
-from tqdm import tqdm
 from RatingSystem import RatingSystem
 
-
 class MySystem(RatingSystem):
-
     def __init__(self):
         super().__init__()
+        
         all_ratings = [r for ratings in self.movie_ratings.values() for r in ratings]
-        self.global_avg = np.mean(all_ratings) if all_ratings else 2.5
+        self.global_avg = np.mean(all_ratings) if all_ratings else 3
         self.movie_means = {}
         for m_id, ratings in self.movie_ratings.items():
-            self.movie_means[m_id] = np.mean(ratings) if ratings else self.global_avg
+            n = len(ratings)
+            # Im więcej ocen, tym bardziej ufamy średniej filmu
+            self.movie_means[m_id] = (sum(ratings) + 10 * self.global_avg) / (n + 10)
 
     def rate(self, user, movie):
         m_id = movie.id if hasattr(movie, 'id') else int(movie)
-        user_ratings = list(user.ratings.values())
-        user_avg = np.mean(user_ratings) if user_ratings else self.global_avg
         movie_avg = self.movie_means.get(m_id, self.global_avg)
-        prediction = (user_avg + movie_avg) / 2
+        
+        # Obliczamy bias użytkownika
+        user_ratings = list(user.ratings.values())
+        if not user_ratings:
+            return movie_avg
+            
+        u_n = len(user_ratings)
+        user_avg = (sum(user_ratings) + 5 * self.global_avg) / (u_n + 5)
+        raw_user_bias = user_avg - self.global_avg
+        confidence_multiplier = 0.7 
+        adjusted_user_bias = raw_user_bias * confidence_multiplier
+        
+        prediction = movie_avg + adjusted_user_bias
         return max(0.5, min(5.0, float(prediction)))
 
     def __str__(self):
