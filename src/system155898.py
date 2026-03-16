fd352212e5427c860646c7c80bb866e80ca92ae2
aleksandrabamberska <ola_bamberska@wp.pl>
2026-03-16 20:57:23 +0100

    Implementacja systemu opartego na średnich ocenach

--- src/system155898.py
+++ src/system155898.py
@@ -1,21 +1,26 @@
+import numpy as np
+import csv
+from tqdm import tqdm
 from RatingSystem import RatingSystem
 
 
 class MySystem(RatingSystem):
+
     def __init__(self):
         super().__init__()
-        # tutaj wczytać dodatkowe dane
+        all_ratings = [r for ratings in self.movie_ratings.values() for r in ratings]
+        self.global_avg = np.mean(all_ratings) if all_ratings else 2.5
+        self.movie_means = {}
+        for m_id, ratings in self.movie_ratings.items():
+            self.movie_means[m_id] = np.mean(ratings) if ratings else self.global_avg
 
     def rate(self, user, movie):
-        """
-        Ta metoda zwraca rating w skali 1-5. Jest to ocena przyznana przez użytkownika 'user' filmowi 'movie'.
-        """
-        n = len(user.ratings.values())
-        if n == 0:
-            return 3
-        else:
-            return sum(user.ratings.values()) / (n - 1)
-        # tutaj usprawnić działanie 
+        m_id = movie.id if hasattr(movie, 'id') else int(movie)
+        user_ratings = list(user.ratings.values())
+        user_avg = np.mean(user_ratings) if user_ratings else self.global_avg
+        movie_avg = self.movie_means.get(m_id, self.global_avg)
+        prediction = (user_avg + movie_avg) / 2
+        return max(0.5, min(5.0, float(prediction)))
 
     def __str__(self):
         """
